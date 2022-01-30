b9caf89be2d52ec4c4ea74a238b61eb3307aa55b
Maciej Futyma <mfutyma2000@gmail.com>
2022-01-30 16:06:36 +0100

    Server main view

---
+++ server/Views/MainServerView.py
@@ -0,0 +1,163 @@
+
+
+from tkinter import *
+from tkinter import ttk
+import tkinter.messagebox
+from tkinter.constants import NSEW
+import configparser
+
+LOOP_INTERVAL = 1_000
+# CONFIG = "m_config.txt"
+
+
+class MainServerView(Frame):
+
+    def __init__(self, parent):
+        # self.konfig = configparser.ConfigParser()
+        # self.konfig.read(CONFIG, "UTF8")
+        super().__init__(parent)
+        self.parent = parent
+        self.parent.protocol("WM_DELETE_WINDOW", self.file_quit)
+        self.parent.resizable(width=False, height=False)
+        # self.domyslne = self.konfig["DEFAULT"]
+        self.geometria_baza = "840x775+204+118"
+        self.parent.geometry(self.geometria_baza)
+        self.utworz_bazowe_menu()
+        self.utworz_status()
+        self.utworz_okno_robocze()
+        self.gallery()
+        self.parent.columnconfigure(0, weight=999)
+        self.parent.columnconfigure(1, weight=1)
+        self.parent.rowconfigure(0, weight=1)
+        self.parent.rowconfigure(1, weight=9999)
+        self.parent.rowconfigure(2, weight=1)
+
+    def gallery(self):
+        self.color = "SystemButtonFace"
+
+        self.createLayout()
+
+    def createLayout(self):
+        title = Frame(self.robocze, height=100,width=800, background="#fff")
+        title.pack(fill=None, expand=False)
+        title.pack_propagate(0)
+        self.titleVal = Label(title, height=4, text="Podłączeni klienci", font=(
+            "Helvetica", 25), bg="#fff")
+        self.titleVal.pack(fill=None, expand=False)
+        self.createLeftFrame()
+
+    def createLeftFrame(self):
+        temp = Frame(self.robocze, height=700, width=800,
+                              background=self.color, highlightbackground="black", highlightthickness=2)
+        temp.pack()
+        temp.pack_propagate(0)
+        s = Scrollbar(temp)
+        s.pack(side = RIGHT, fill = Y)
+        
+        
+        detectorsData2 = Canvas(temp, height=700, width=800,
+                              background=self.color,yscrollcommand = s.set )
+        detectorsData2.pack()
+        detectorsData2.pack_propagate(0)
+        s.config(command=detectorsData2.yview)
+        
+        detectorsData = ttk.Frame(detectorsData2)
+        
+        detectorsData.bind(
+            "<Configure>",
+            lambda e: detectorsData2.configure(
+                scrollregion=detectorsData2.bbox("all")
+            )
+        )
+        detectorsData2.create_window((0, 0),width=800, window=detectorsData, anchor="nw")
+        
+        for i in range(10) :
+            self.add_client_frame(detectorsData);
+        
+       
+
+    def add_client_frame(self,frame):
+        clientFrame = Frame(frame, height=120, width=800,
+                              background="#fff", highlightbackground="black", highlightthickness=1)
+        clientFrame.pack()
+        clientFrame.pack_propagate(0)
+        idLabel = Label(clientFrame, height=2,
+                             text="Id:", font=("Helvetica", 19), bg="#fff")
+        idLabel.pack(side=LEFT)
+        idValLabel = Label(clientFrame,  height=2,
+                             text="254657", font=("Helvetica", 19), bg="#fff")
+        idValLabel.pack(side=LEFT)
+        nameLabel = Label(clientFrame,  height=2,
+                             text=", Nazwa:", font=("Helvetica", 19), bg="#fff")
+        nameLabel.pack(side=LEFT)
+        nameValLabel = Label(clientFrame,  height=2,
+                             text="Galeria handlowa Amber", font=("Helvetica", 19), bg="#fff")
+        nameValLabel.pack(side=LEFT)
+        detailsButton = Button(clientFrame, text="Szczegóły", font=("Helvetica", 20), height=1, width=12,
+                                    bg=self.color,)
+        detailsButton.pack(side=RIGHT, padx=30)
+    # Creates status bar
+
+    def utworz_status(self):
+        self.statusbar = Label(self.parent, text="Linia statusu...",
+                               anchor=tkinter.W)
+        self.statusbar.after(5000, self.clearStatusBar)
+        self.statusbar.grid(row=2, column=0, columnspan=2,
+                            sticky=tkinter.EW)
+
+    # Sets status bar with given message
+    def ustawStatusBar(self, txt):
+        self.statusbar["text"] = txt
+
+    # Clears message in status bar
+    def clearStatusBar(self):
+        self.statusbar["text"] = ""
+
+    # Creates basic menu "File" in menu bar
+    def utworz_bazowe_menu(self):
+        self.menubar = Menu(self.parent)
+        self.parent["menu"] = self.menubar
+        fileMenu = Menu(self.menubar)
+        for label, command, shortcut_text, shortcut in (
+                ("Save", self.file_save, "Ctrl+S", "<Control-s>"),
+                ("Quit", self.file_quit, "Ctrl+Q", "<Control-q>")):
+            if label is None:
+                fileMenu.add_separator()
+            else:
+                fileMenu.add_command(label=label, underline=0,
+                                     command=command, accelerator=shortcut_text)
+                self.parent.bind(shortcut, command)
+        self.menubar.add_cascade(label="File", menu=fileMenu, underline=0)
+        pass
+
+    # Creates basic menu "Help" in menu bar
+    def dodaj_menu_help(self):
+        pass
+
+    # Creates basic menu "Edit" in menu bar
+    def dodaj_menu_edit(self):
+        pass
+
+    # Ends program
+    def file_quit(self, event=None):
+        reply = tkinter.messagebox.askyesno(
+            "koniec pracy",
+            "naprawdę kończysz?", parent=self.parent)
+        event = event
+        if reply:
+            # with open(CONFIG, 'w') as config_plik:
+            #     self.konfig.write(config_plik)
+            self.parent.destroy()
+        pass
+
+    # Saves configuration in config file
+    def file_save(self, event=None):
+        event = event
+        pass
+
+    # Creates basic graphic workspace for game gui
+    def utworz_okno_robocze(self):
+        self.robocze = Frame(self.parent, background='#ffffff')
+        self.robocze.grid(row=1, column=0, columnspan=1,
+                          rowspan=1, sticky=NSEW)
+        pass
--- server/app.py
+++ server/app.py
@@ -1,4 +1,22 @@
 import database as db
 
+from tkinter import *
+from Views.MainServerView import MainServerView
+
+LOOP_INTERVAL = 500
 if __name__ == '__main__':
-    db.create_database()
+    #db.create_database()
+    root = Tk()
+    root.title("System monitorowania środowisk galerii")
+
+    view = MainServerView(root)
+
+    def loop():
+        print("main loop tick")
+
+    def runLoop():
+        loop()
+        root.after(LOOP_INTERVAL, runLoop)
+
+    root.after(LOOP_INTERVAL, runLoop)
+    root.mainloop()
